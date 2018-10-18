b46d8f7556b54d708a2899b9c87f37116c6a8e34
BoBOHanLee <41977104+BoBOHanLee@users.noreply.github.com>
2018-10-18 18:47:08 +0800

    2018/10/18
    以多進程方式一直讀取圖片

---
+++ ROI_process.py
@@ -0,0 +1,73 @@
+# to mark out the position of nozzle and output ROI
+from __future__ import print_function
+import cv2
+import numpy as np
+
+MAX_FEATURES = 500
+GOOD_MATCH_PERCENT = 0.1  #取匹配效果最好的前10%
+
+# nozzle tracking for feature match
+def alignImages(im1, im2):
+    # Convert images to grayscale
+    im1Gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+    im2Gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+
+    # Detect ORB features and compute descriptors.
+    orb = cv2.ORB_create(MAX_FEATURES)
+    keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
+    keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
+
+    # Match features.
+    matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)    # use the way of Brute-Force
+    matches = matcher.match(descriptors1, descriptors2, None)
+
+    # Sort matches by score
+    matches.sort(key=lambda x: x.distance, reverse=False)  #將匹配分數由最好排到最低
+
+    # Remove not so good matches
+    numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
+    matches = matches[:numGoodMatches]
+
+
+    # Draw top matches
+    imMatches = cv2.drawMatches(im1, keypoints1, im2, keypoints2, matches, None)
+    #cv2.imwrite("matches.jpg", imMatches)
+
+    # Extract location of good matches
+    points1 = np.zeros((len(matches), 2), dtype=np.float32)      # ( x,y )
+    points2 = np.zeros((len(matches), 2), dtype=np.float32)
+
+    for i, match in enumerate(matches):                          # enumerate 索引以及索引對應到的值
+        points1[i, :] = keypoints1[match.queryIdx].pt
+        points2[i, :] = keypoints2[match.trainIdx].pt
+
+    #print(points1)
+    # Find homography   轉換矩陣 以匹配點來計算
+    h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+
+    # Use homography
+    height, width, channels = im2.shape
+    im1Reg = cv2.warpPerspective(im1, h, (width, height))
+
+
+
+
+    # cal. the most lower nozzle point
+    cup=0
+    num_xy=0
+    for i , XY in enumerate(points2):
+        if XY[1] > cup:
+            cup = XY[1]
+            num_xy=i
+        else:
+            continue
+
+    cv2.circle(im2, (points2[num_xy,0],points2[num_xy,1]) ,50, (0, 0, 255), -1)
+    #im2 = cv2.resize(im2, None, fx=0.3, fy=0.3, interpolation=cv2.INTER_LINEAR)
+    #cv2.imshow('nozzle',im2)
+    cv2.imwrite('nozzle.jpg',im2)
+
+    
+
+    return im1Reg, h
+
--- Ui_MainWindow.py
+++ Ui_MainWindow.py
@@ -4,17 +4,22 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QFileDialog, QMessageBox, QDockWidget, QListWidget
 from PyQt5.QtGui import *
-
+import threading
+import time
 
 from UI_design.test import Ui_MainWindow
 from UI_design.close_dialog import Ui_Dialog as Ui_Dialog_close
+import ROI_process
+
+
+
 
 class mywindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def __init__(self):
         super(mywindow, self).__init__()
         self.setupUi(self)
-        self.show_bottom.clicked.connect(self.showImage_fuction)   #shownig image
+        self.show_bottom.clicked.connect(self.threading_showImage)   #以多線程 跑shownig image
         self.close_botton.clicked.connect(self.click_closeButton)
 
 
@@ -28,52 +33,66 @@ class mywindow(QtWidgets.QMainWindow, Ui_MainWindow):
         ui.setupUi(Dialog)
         Dialog.show()
         #Dialog.exec_()
+
         rsp = Dialog.exec_()
         if rsp == QtWidgets.QDialog.Accepted:   #，QtWidgets.QDialog.Accepted  對話框的接收事件
-            self.label_closeDialog.setText("點了OK")
+            self.close()
         else:
-            self.label_closeDialog.setText("點了Cannel")
+            self.show()
 
+
+    #背景一直顯示圖片和roi
     def showImage_fuction(self):      # for showing image
+        while True:                   #以多進程方式每隔一秒就讀取顯示
+            time.sleep(1)
+            # -----------------------Image Processing-----------------------------#
+
+            # nozzle tracking
+            im = cv2.imread("test_photo/head.jpg", cv2.IMREAD_COLOR)
+            imReference = cv2.imread("test_photo/3.jpg", cv2.IMREAD_COLOR)  # 1.jpg就是之後樹梅派要一直傳進來的圖片
+            __, h = ROI_process.alignImages(im, imReference)
+            print("Estimated homography : \n", h)
+
+            # main
+            img = cv2.imread('nozzle.jpg')
+            img = cv2.resize(img, (261, 441))
+            image_height, image_width, image_depth = img.shape
+
+            # roi
+            img_roi = cv2.imread('roi.jpg')
+            img_roi = cv2.resize(img_roi, (111, 61))
+            roi_height, roi_width, roi_depth = img_roi.shape
+
+            # -------------- Qimge to show on the interface-----------------------#
+            # main
+            QImg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            QImg = QImage(QImg.data, image_width, image_height,
+                          image_width * image_depth,
+                          QImage.Format_RGB888)
+            self.img_main.setPixmap(QPixmap.fromImage(QImg))
+
+            # roi
+            QImg_roi = cv2.cvtColor(img_roi, cv2.COLOR_BGR2RGB)
+            QImg_roi = QImage(QImg_roi.data, roi_width, roi_height,
+                              roi_width * roi_depth,
+                              QImage.Format_RGB888)
+            self.img_roi.setPixmap(QPixmap.fromImage(QImg_roi))
+            # ---------------------------------------------------------------------#
+
+    def threading_showImage(self):
+          thread = threading.Thread(target=self.showImage_fuction)  # 定义线程
+          thread.start()  # 让线程开始工作
 
 
-
-        #-----------------------Image Processing-----------------------------#
-        #main
-        img = cv2.imread('fail.jpg')
-        image_height, image_width, image_depth = img.shape
-
-        #roi
-        img_roi = cv2.imread('roi.jpg')
-        roi_height, roi_width, roi_depth = img_roi.shape
-
-
-
-
-
-
-
-        #-------------- Qimge to show on the interface-----------------------#
-        #main
-        QImg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        QImg = QImage(QImg.data, image_width, image_height,
-                     image_width * image_depth,
-                     QImage.Format_RGB888)
-        self.img_main.setPixmap(QPixmap.fromImage(QImg))
-
-        #roi
-        QImg_roi = cv2.cvtColor(img_roi, cv2.COLOR_BGR2RGB)
-        QImg_roi = QImage(QImg_roi.data, roi_width, roi_height,
-                      roi_width * roi_depth,
-                      QImage.Format_RGB888)
-        self.img_roi.setPixmap(QPixmap.fromImage(QImg))
-        #---------------------------------------------------------------------#
 
 
 
 
 
 if __name__ == '__main__':
+
+
+
     app = QtWidgets.QApplication(sys.argv)
     window = mywindow()
     window.show()
